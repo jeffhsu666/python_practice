7fd0a4d18ece94cc3f852afe1c7c25366cc31ae0
Jeff Hsu <sadeibear@gmail.com>
2020-05-24 11:06:17 +0800

    temperature transfer practice

---
+++ c2f_transfer.py
@@ -0,0 +1,6 @@
+# Celsius to Fahrenheit transfer
+c_degree = input('type in a temperature in Celsius: ')
+
+c_degree = float(c_degree)
+f_degree = c_degree * 1.8 + 32
+print('Celsius ', c_degree, 'degrees = Fahrenheit ', f_degree, 'degrees.')
